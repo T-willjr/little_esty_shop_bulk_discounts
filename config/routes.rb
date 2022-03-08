7cee645175fd9b41e8030456abfe78113ab0a10e
TJ Williams <williamsjr.timothy1@gmail.com>
2022-03-08 12:15:34 -0500

    Add: Routes and controller for bulk discounts

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,10 @@ Rails.application.routes.draw do
     resources :items, except: [:destroy]
     resources :item_status, only: [:update]
     resources :invoices, only: [:index, :show, :update]
+    resources :bulk_discounts, only: [:index, :show]
   end
+
+  get "/merchant/:id/:bulk_discount_id", to: "bulk_discounts#show"
 
   namespace :admin do
     resources :dashboard, only: [:index]
