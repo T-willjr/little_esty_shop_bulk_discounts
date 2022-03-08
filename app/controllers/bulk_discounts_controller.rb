7184e171fe1d5c5a227ae245b76b5034e3b184a8
TJ Williams <williamsjr.timothy1@gmail.com>
2022-03-08 12:33:58 -0500

    Add: Holiday Search to bulk discount controller

--- app/controllers/bulk_discounts_controller.rb
+++ app/controllers/bulk_discounts_controller.rb
@@ -1,6 +1,9 @@
+require './lib/holiday_search'
+
 class BulkDiscountsController < ApplicationController
   def index
     @merchant = Merchant.find(params[:merchant_id])
+    @holidays = HolidaySearch.new
   end
 
   def show
